f83058b74fe940d408c7a0c6d558b886e5a797af
Fernando Araujo <fernarau9652@gmail.com>
2024-03-26 12:15:27 -0600

    Implemented the initial peripherals for Timers, DAC, DMA, I2C, and LEDs. This is only a template for later testing.

--- STM32/5780-6780-Final-Project/Core/Src/main.c
+++ STM32/5780-6780-Final-Project/Core/Src/main.c
@@ -19,42 +19,14 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
+void initTIM(void);
+void initDAC(void);
+void initDMA(void);
+void initLEDs(void);
+void initI2C(void);
 
-/* USER CODE END Includes */
-
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
-
-/* USER CODE END PTD */
-
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
-
-/* USER CODE END PD */
-
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-
-/* USER CODE END PM */
-
-/* Private variables ---------------------------------------------------------*/
-
-/* USER CODE BEGIN PV */
-
-/* USER CODE END PV */
-
-/* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
-/* USER CODE BEGIN PFP */
 
-/* USER CODE END PFP */
-
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
-
-/* USER CODE END 0 */
 
 /**
   * @brief  The application entry point.
@@ -62,41 +34,100 @@ void SystemClock_Config(void);
   */
 int main(void)
 {
-  /* USER CODE BEGIN 1 */
-
-  /* USER CODE END 1 */
-
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
   SystemClock_Config();
+	
+	// Enable GPIOA, GPIOB, GPIOC, I2C, TIM2, and TIM3 Clock in RCC (We can adjust these later)
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+	__HAL_RCC_GPIOB_CLK_ENABLE();
+	__HAL_RCC_GPIOC_CLK_ENABLE();
+	__HAL_RCC_I2C2_CLK_ENABLE();
+	__HAL_RCC_TIM2_CLK_ENABLE();
+	__HAL_RCC_TIM3_CLK_ENABLE();
+	
+	// Initialize LEDs for debugging I2C
+	initLEDs();
+	
+	// Initialize timers for the intended 42 kHz target.
+	initTIM();
+	
+	
 
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
-  /* USER CODE BEGIN 2 */
-
-  /* USER CODE END 2 */
-
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
+  
   while (1)
   {
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
+    
   }
-  /* USER CODE END 3 */
+}	// END main
+
+
+
+/* Initialize Timer to the Nyquist sampling rate */
+void initTIM(void) {
+	// Configure the timer (TIM2) to trigger an update event (UEV) at 42 kHz
+	TIM2->PSC = 0x1;  // 0.9524 ~1
+	TIM2->ARR = 0xBD; // 189.48 ~189
+	
+	// Configure the timer to generate an interrupt on the UEV event
+	TIM2->DIER |= TIM_DIER_UIE;
+	
+	// Configure an enable/start the timer 2
+	TIM2->CR1 |= TIM_CR1_CEN;
+	
+	// Eventually use TIM3 for NVIC if interrupts need to be used
+
 }
+
+
+
+/* Initialize DAC */
+void initDAC(void) {
+
+
+}
+
+
+
+/* Initialize DMA */
+void initDMA(void) {
+
+
+}
+
+
+
+/* Initialize I2C */
+void initI2C(void) {
+
+
+}
+
+
+/* Initialize the LEDs for debugging purposes*/
+void initLEDs(void) {	
+	/* Initialize all LEDs: RED (PC6), BLUE (PC7), ORANGE (PC8), GREEN (PC9)	*/
+	// (Reset state: 00)
+	GPIOC->MODER &= ~(GPIO_MODER_MODER6_Msk | GPIO_MODER_MODER7_Msk | GPIO_MODER_MODER8_Msk | GPIO_MODER_MODER9_Msk);
+	
+	// (General purpose: 01) 
+	GPIOC->MODER |= (GPIO_MODER_MODER6_0 | GPIO_MODER_MODER7_0 | GPIO_MODER_MODER8_0 | GPIO_MODER_MODER9_0);
+	
+	// Configure Push/Pull Output type for PC6, PC7, PC8, and PC9	(00)
+	GPIOC->OTYPER &= ~(GPIO_OTYPER_OT_6 | GPIO_OTYPER_OT_7 | GPIO_OTYPER_OT_8 | GPIO_OTYPER_OT_9);
+	
+	// Configure low speed for PC6, PC7, PC8, and PC9	(00)
+	GPIOC->OSPEEDR &= ~(GPIO_OSPEEDR_OSPEEDR6_Msk | GPIO_OSPEEDR_OSPEEDR7_Msk | GPIO_OSPEEDR_OSPEEDR8_Msk | GPIO_OSPEEDR_OSPEEDR9_Msk);
+	
+	// Configure no pull-up/down resistors for PC6, PC7, PC8, and PC9	(00)
+	GPIOC->PUPDR &= ~(GPIO_PUPDR_PUPDR6_Msk | GPIO_PUPDR_PUPDR7_Msk | GPIO_PUPDR_PUPDR8_Msk | GPIO_PUPDR_PUPDR9_Msk);
+	
+	// Initialize pins to logic high and the other to low.
+	GPIOC->BSRR = GPIO_BSRR_BR_6;	// Set PC6 low
+	GPIOC->BSRR = GPIO_BSRR_BR_7; // Set PC7 low
+	GPIOC->BSRR = GPIO_BSRR_BR_8;	// Set PC8 low
+	GPIOC->BSRR = GPIO_BSRR_BR_9; // Set PC9 low
+}
+
 
 /**
   * @brief System Clock Configuration
